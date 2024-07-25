2ad01c002dfddbbc8fe1a10fa57662e86f8b92e6
beqa20088 <138366831+beqa20088@users.noreply.github.com>
2024-07-25 14:49:33 +0400

    asdasd

---
+++ script.js
@@ -0,0 +1,6 @@
+console.log('Name is : Beqa')
+console.log('Surname is : Dididze')
+console.log('Age is : 15')
+console.warn('Email : beqadididze2@gmail.com')
+console.warn('Password : safhuhaiuh')
+console.log('Phone : 599475225')
